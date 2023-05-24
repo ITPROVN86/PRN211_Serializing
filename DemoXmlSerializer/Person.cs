a11de6400bfba4ca539167d7406c79a292006e22
ITPROVN86 <quangltn3@fe.edu.vn>
2023-05-24 16:23:16 +0700

    Update Console App XMLSerializing

---
+++ DemoXmlSerializer/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DemoXmlSerializer
+{
+    [XmlRoot("Candidate")]
+    public class Person
+    {
+        [XmlElement("FirtName")]
+        public string Name { get; set; }
+        [XmlElement("RouthAge")]
+        public int Age { get; set; }
+    }
+}
---
+++ DemoXmlSerializer/Program.cs
@@ -0,0 +1,17 @@
+﻿using DemoXmlSerializer;
+using System.Xml.Serialization;
+
+Person p1 = new Person() { Name = "David", Age = 34 };
+var xs = new XmlSerializer(typeof(Person));
+//Serialize
+using Stream s1 = File.Create("persion.xml");
+xs.Serialize(s1, p1);
+s1.Close();
+//Deserialize
+using Stream s2 = File.OpenRead("persion.xml");
+var p2 = (Person)xs.Deserialize(s2);
+Console.WriteLine("***Person Info***");
+Console.WriteLine($"Name: {p2.Name}, Age: {p2.Age}");
+Console.WriteLine("***persion.xml***");
+string xmlData = File.ReadAllText("persion.xml");
+Console.WriteLine(xmlData);
