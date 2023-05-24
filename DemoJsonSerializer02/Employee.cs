98cb4ce9755c6d00c6af23aba9427b802669c41d
ITPROVN86 <quangltn3@fe.edu.vn>
2023-05-24 21:58:43 +0700

    DemoJsonSerializer02

---
+++ DemoJsonSerializer02/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DemoJsonSerializer02
+{
+    public class Employee
+    {
+        [JsonPropertyName("FullName")]
+        public string Name { get; set; }
+        [JsonIgnore]
+        public string Email { get; set; }
+        [JsonInclude]
+        public decimal Salary { get; set; }
+    }
+}
---
+++ DemoJsonSerializer02/Program.cs
@@ -0,0 +1,15 @@
+﻿using DemoJsonSerializer02;
+using System.Text.Json;
+string json = @"{
+""FullName"":""Mark"",
+""Email"":""Mark@gmail.com"",
+""Salary"":1000,
+}";
+var option = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    ReadCommentHandling = JsonCommentHandling.Skip,
+    AllowTrailingCommas = true
+};
+var emp = JsonSerializer.Deserialize<Employee>(json,option);
+Console.WriteLine($"Name: {emp.Name}, Email: {emp.Email}, Salary: {emp.Salary}");
